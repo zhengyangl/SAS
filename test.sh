507712750c2447793443afaf6625c78d99a240f9
Filip Bartek <filip.bartek@cern.ch>
2013-07-19 20:06:33 +0200

    test.sh: Added "-j 32" Now performs build on 32 CPUs instead of just one.

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@
 
 cd test
 #lcg-scan-build -v -v -v -o report -enable-checker threadsafety make -B
-lcg-scan-build -o report -enable-checker threadsafety make -B
+lcg-scan-build -o report -enable-checker threadsafety make -B -j 32
 cd -
