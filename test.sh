ed66a4f3f844ecc8a7765151ad8218102c73e673
Filip Bartek <filip.bartek@cern.ch>
2013-07-19 17:57:51 +0200

    test.sh: Removed the verbose arguments of scan-build so that the output is briefer.

--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@
 # Test the plugin on example files.
 
 cd test
-lcg-scan-build -v -v -v -o report -enable-checker threadsafety make -B
+#lcg-scan-build -v -v -v -o report -enable-checker threadsafety make -B
+lcg-scan-build -o report -enable-checker threadsafety make -B
 cd -
