1ae9633d8f3db0a9d6a06f7810304e19706e125c
Filip Bartek <filip.bartek@cern.ch>
2013-07-31 19:13:19 +0200

    Unregistered the 3 checkers removed in previous commit. Registration removed from "src/ClangSasCheckerPluginRegister.cpp".

--- src/ClangSasCheckerPluginRegister.cpp
+++ src/ClangSasCheckerPluginRegister.cpp
@@ -11,10 +11,7 @@
 #include "GlobalStaticChecker.h"
 #include "StaticLocalChecker.h"
 #include "MutableMemberChecker.h"
-#include "ClassChecker.h"
 #include "ClassDumper.h"
-#include "edmChecker.h"
-#include "getByChecker.h"
 #include "FiniteMathChecker.h"
 #include "CatchAll.h"
 #include "UsingNamespace.h"
@@ -33,15 +30,12 @@ void clang_registerCheckers ( clang::ento::CheckerRegistry &registry)
 	registry.addChecker< sas::StaticLocalChecker>( "threadsafety.StaticLocal", "Checks for non-const method local statics which might not be thread-safe" );
 	registry.addChecker< sas::MutableMemberChecker>( "threadsafety.MutableMember", "Checks for members with the mutable keyword which might not be thread-safe" );
 	registry.addChecker< sas::GlobalStaticChecker>( "threadsafety.GlobalStatic", "Checks for global non-const statics which might not be thread-safe" );
-	registry.addChecker< sas::ClassChecker>( "optional.ClassChecker", "Checks classes of interest" );
 	registry.addChecker< sas::ClassDumperCT>( "optional.ClassDumperCT", "Dumps class info" );
 	registry.addChecker< sas::ClassDumperFT>( "optional.ClassDumperFT", "Dumps class info" );
 	registry.addChecker< sas::ClassDumperInherit>( "optional.ClassDumperInherit", "Dumps class inheritance info" );
 	registry.addChecker< sas::FiniteMathChecker>( "sas.NonFiniteMath", "Reports usage of isnan and isinf." );
 	registry.addChecker< sas::UsingNamespace>( "sas.CodeRules.UsingNamespace", "Checks for 'using namespace' or 'using std::' in header files" );
 	registry.addChecker< sas::CatchAll>( "sas.CodeRules.CatchAll", "Checks for 'catch(...)' in source files" );
-	registry.addChecker< sas::edmChecker>( "optional.edmChecker", "Checks classes inheriting from edm::Producer and edm::Filter" );
-	registry.addChecker< sas::getByChecker>( "optional.getByChecker", "Checks for calls to edm::getByLabel or edm::getManyByType and reports edm::Handle type passed" );
 	registry.addChecker< sas::ArgSizeChecker>( "sas.ArgSize", "Reports args passed by value with size>4k." );
 }
 
