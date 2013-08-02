60cd51e7fca1d319994e5861d9b4eacb5a449e0d
Filip Bartek <filip.bartek@cern.ch>
2013-08-02 14:55:57 +0200

    src/UsingNamespace.cpp: Added comments in code documenting what I think is the meaning of the code.

--- src/UsingNamespace.cpp
+++ src/UsingNamespace.cpp
@@ -9,26 +9,29 @@
 #include "SasSupport.h"
 using namespace sas;
 
+// using namespace *
 void UsingNamespace::checkASTDecl (const clang::UsingDirectiveDecl *D, clang::ento::AnalysisManager &Mgr, clang::ento::BugReporter &BR) const
 {
   if (isDeclOK(D,BR)) return;
   reportBug("'using namespace '",D,BR);
 }
 
+// using std::*
 void UsingNamespace::checkASTDecl (const clang::UsingDecl *D, clang::ento::AnalysisManager &Mgr, clang::ento::BugReporter &BR) const
 {
   if (isDeclOK(D,BR)) return;
   std::string NS = D->getQualifier ()->getAsNamespace ()->getNameAsString ();
-  if (strcmp(NS.c_str(),"std")!=0) return;
+  if (strcmp(NS.c_str(),"std")!=0) return; // Different from "std"
   reportBug("'using std:: '",D,BR);
 }
 
+// Not OK (returns false) when in a header file and top block
 bool UsingNamespace::isDeclOK (const clang::NamedDecl *D, clang::ento::BugReporter &BR) const
 {
-  if (D->getDeclContext ()->getParent()!=0) return true;
-  const char *sfile=BR.getSourceManager().getPresumedLoc(D->getLocation ()).getFilename();
+  if (D->getDeclContext ()->getParent()!=0) return true; // Not on top level in AST
+  const char *sfile=BR.getSourceManager().getPresumedLoc(D->getLocation ()).getFilename(); // Get filename
   size_t flen = strlen(sfile);
-  if ((sfile[flen-2] != '.') || (sfile[flen-1] != 'h')) return true;
+  if ((sfile[flen-2] != '.') || (sfile[flen-1] != 'h')) return true; // Not in a header file (*.h)
   return false;
 }
 
