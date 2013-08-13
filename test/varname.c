4c0d2bff927c6569a5002313be828063a34cd27d
Filip Bartek <fbartek@cern.ch>
2013-08-13 15:25:11 +0200

    "test/varname.c": Added header with basic information.

--- test/varname.c
+++ test/varname.c
@@ -1,3 +1,8 @@
+// Filip Bartek <filip.bartek@cern.ch>
+
+// Test case for the checker sas.Varname
+// Bugs: 1
+
 int main(void) {
   int variable; // bug: doesn't begin with uppercase
   int Variable; // not bug: begins with uppercase
