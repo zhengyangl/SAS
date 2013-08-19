b439965da39d6733cc41ce62022911cb0e0e24ad
Filip Bartek <fbartek@cern.ch>
2013-08-19 18:49:28 +0200

    Added src/dbgs.h: Defines a llvm::raw_ostream for debug output (redirected to standard output iff NDEBUG is not #defined).

---
+++ src/dbgs.h
@@ -0,0 +1,40 @@
+// -*-c++-*-
+
+// Defines a raw_ostream for debug output.
+// Access the ostream as `sas::dbgs()`.
+// Use it the same way as (for example) `llvm::outs()`, namely:
+//   dbgs() << "Hello world!\n";
+
+// If the macro NDEBUG is defined, `dbgs` redirects the stream to standard
+// output. Otherwise, it redirects the stream to `nulls`, a null stream (dumping
+// the data).
+
+// Filip Bartek <filip.bartek@cern.ch>
+
+#ifndef SAS_DBGS_H
+#define SAS_DBGS_H
+
+// Declarations:
+
+#include <llvm/Support/raw_ostream.h>
+// llvm::raw_ostream
+
+namespace sas {
+  static llvm::raw_ostream& dbgs();
+} // end namespace sas
+
+// Definitions:
+
+#include <llvm/Support/raw_ostream.h>
+// llvm::outs
+// llvm::nulls
+
+llvm::raw_ostream& sas::dbgs() {
+#ifdef NDEBUG
+  return llvm::nulls();
+#else
+  return llvm::outs();
+#endif // NDEBUG
+}
+
+#endif
