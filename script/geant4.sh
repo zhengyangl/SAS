c96073b9b1c44093b676e31a6bb69012c85251e4
Filip Bartek <fbartek@cern.ch>
2013-08-19 15:19:26 +0200

    geant4.sh: Verbatim cmake output, disable sas.CodeRules.CatchAll, removed arg "-disable-checker sas.example.Varname" because Varname is implicitly disabled by new setup.sh.

--- script/geant4.sh
+++ script/geant4.sh
@@ -12,7 +12,10 @@ tar -zxf geant4.9.6.p02.tar.gz
 mkdir -p geant4-build
 
 cd geant4-build
-sas-scan-build-none -o ../report cmake -DCMAKE_BUILD_TYPE=Debug ../geant4.9.6.p02
-# In case of error: add the -D(install) arg
-sas-scan-build-sas -o ../report -disable-checker sas.Varname make
+sas-scan-build -v -v -v -o ../report cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$OLDPWD/geant4-install ../geant4.9.6.p02
+sas-scan-build-sas -o ../report -disable-checker sas.CodeRules.CatchAll make -j 20
 cd -
+
+
+#lcg-scan-build -v -v -v -o $SAS_REPORT_PATH/geant4-cmake cmake -DCMAKE_INSTALL_PREFIX=$OLDPWD/install $OLDPWD/geant4.9.6.p02 -DCMAKE_BUILD_TYPE=Debug
+#lcg-scan-build -o $SAS_REPORT_PATH/geant4-make-stable -enable-checker core -enable-checker cplusplus -enable-checker deadcode -enable-checker llvm -enable-checker security -enable-checker unix make -j 20
