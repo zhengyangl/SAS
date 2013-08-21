50a24bb2a23b25223453295df2468986a1cab8e6
Filip Bartek <fbartek@cern.ch>
2013-08-21 15:58:54 +0200

    test/const_cast.cpp: (cosmetic) Reformatted whitespace.

--- test/const_cast.cpp
+++ test/const_cast.cpp
@@ -4,21 +4,18 @@
 // Test case for the checker "threadsafety.ConstCast" (2 bugs)
 // Also triggers the checker "threadsafety.ConstCastAway" (2 bugs)
 
-
 #include <string>
 std::string;
 
-
 int main()
 {
-    string s = "23";
-    string const& r_const = s;
+  string s = "23";
+  string const& r_const = s;
     
-    // will produce a warning by ConstCastChecker
-    // and ConstCastAwayChecker
-    string & r = const_cast< string & >( r_const );
+  // will produce a warning by ConstCastChecker
+  // and ConstCastAwayChecker
+  string & r = const_cast< string & >( r_const );
+  string & r2 = const_cast< string & >( r_const ); // bug
 
-    string & r2 = const_cast< string & >( r_const ); // bug
-
-    return 0;
+  return 0;
 }
