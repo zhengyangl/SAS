285f5cf84bdbe971cb07ba1e1951c16851a61a5f
Filip Bartek <fbartek@cern.ch>
2013-08-21 21:19:22 +0200

    CheckerDisabler: Improved Stmt disabler: Disables checker if disabler string is in common comment on the line preceding the statement. Warning: Doesn't work on the second line in the file. Note: Doesn't work on the first line in the file, as there's no line preceding it.

--- src/CheckerDisabler.cpp
+++ src/CheckerDisabler.cpp
@@ -1,5 +1,7 @@
 // Filip Bartek <filip.bartek@cern.ch>
 
+#define CHECKERDISABLER_PARENTDECL
+
 #include "CheckerDisabler.h"
 
 #include <clang/AST/DeclBase.h>
@@ -46,20 +48,24 @@ using clang::ento::CheckerContext;
 #include <clang/Analysis/AnalysisContext.h>
 using clang::LocationContext;
 
-#include "dbgs.h"
-using sas::dbgs;
+#include <clang/Basic/SourceLocation.h>
+using clang::SourceLocation;
+using clang::FileID;
 
 namespace {
   bool IsCommentedWithString(const Decl * const decl,
                              const StringRef commentString);
   string FormDisablerString(const StringRef checkerName);
+  bool IsDisabledByParentDecl(const Stmt * const stmt,
+                              const CheckerContext& checkerContext,
+                              const StringRef checkerName);
+  bool IsDisabledBySpecial(const Decl * const decl,
+                           const StringRef checkerName);
 }
 
 bool sas::IsDisabled(const Decl * const decl,
                      const StringRef checkerName) {
-  string commentString = FormDisablerString(checkerName);
-  const StringRef commentStringRef(commentString);
-  return IsCommentedWithString(decl, commentStringRef);
+  return IsDisabledBySpecial(decl, checkerName);
 }
 
 bool sas::IsDisabled(const DeclStmt * const declStmt,
@@ -83,39 +89,49 @@ bool sas::IsDisabled(const Stmt * const stmt,
 {
   if (!stmt)
     return false; // Invalid stmt
-  const LocationContext * const locationContext =
-    checkerContext.getLocationContext();
-  const Decl * const decl = locationContext->getDecl();
-  return IsDisabled(decl, checkerName);
 
-#if 0
-  SourceManager& sourceManager = checkerContext.getSourceManager();
-  // TODO: Implement.
-  dbgs() << "IsDisabled(Stmt*, ...)\n";
-  //dbgs() << "dump:begin\n";
-  //stmt->dump(dbgs(), sourceManager);
-  //dbgs() << "dump:end\n";
-  //clang::SourceRange sourceRange = stmt->getSourceRange();
-  clang::SourceLocation locStart = stmt->getLocStart();
-  locStart.print(dbgs(), sourceManager);
-  dbgs() << "\n";
-  unsigned lineNumber = sourceManager.getSpellingLineNumber(locStart);
-  dbgs() << lineNumber << "\n";
-  clang::ASTContext& astContext = checkerContext.getASTContext();
-  const clang::RawCommentList commentList = astContext.getRawCommentList();
-  llvm::ArrayRef<clang::RawComment*> comments = commentList.getComments();
-  llvm::ArrayRef<clang::RawComment*>::iterator b = comments.begin();
-  llvm::ArrayRef<clang::RawComment*>::iterator e = comments.end();
-  dbgs() << "<comments>\n";
-  for (llvm::ArrayRef<clang::RawComment*>::iterator i = b; i != e; ++i) {
-    const StringRef text = (*i)->getRawText(sourceManager);
-    dbgs() << "<text>\n";
-    dbgs() << text.data();
-    dbgs() << "</text>\n";
-  }
-  dbgs() << "</comments>\n";
+#ifdef CHECKERDISABLER_PARENTDECL
+  if (IsDisabledByParentDecl(stmt, checkerContext, checkerName))
+    return true; // Parent declaration disabled
+#endif // CHECKERDISABLER_PARENTDECL
+
+  const SourceManager& sourceManager = checkerContext.getSourceManager();
+  const SourceLocation stmtLoc = stmt->getLocStart();
+  const unsigned stmtLineNum = sourceManager.getSpellingLineNumber(stmtLoc);
+  if (stmtLineNum < 2) // FIXME: Uses 2 preceding lines instead of 1.
+    return false; // Not enough preceding lines
+  const FileID fileID = sourceManager.getFileID(stmtLoc);
+  // Warning: Line numbers are shifted by `-1` on input to `translateLineCol`
+  // or `getCharacterData` (see further in this file).
+  // [FB] Reason: unknown. (Clang bug?)
+  // TODO: Investigate.
+  const unsigned beginLine = stmtLineNum - 2;
+  // FIXME: Uses 2 preceding lines instead of just one.
+  // [FB] hasn't found a way to use just one.
+  const unsigned beginCol = 0;
+  const unsigned endLine = stmtLineNum - 1;
+  const unsigned endCol = 0;
+  // Warning: Column argument of `translateLineCol` has no effect on resulting
+  // `char *` pointers (`begin`, `end`).
+  const SourceLocation locBegin =
+    sourceManager.translateLineCol(fileID, beginLine, beginCol);
+  const SourceLocation locEnd =
+    sourceManager.translateLineCol(fileID, endLine, endCol);
+  const char * begin = sourceManager.getCharacterData(locBegin) + 1;
+    // `+ 1` gets rid of the '\n' in the beginning of the string
+  const char * end = sourceManager.getCharacterData(locEnd);
+  assert(end >= begin);
+  const string lineString = string(begin, end - begin);
+  const StringRef lineStringRef = StringRef(lineString);
+  const size_t commentCol = lineStringRef.find("//");
+  if (commentCol == npos)
+    return false; // No `//` comment on this line
+  const StringRef commentContent = lineStringRef.substr(commentCol + 2);
+  const string disablerString = FormDisablerString(checkerName);
+  const size_t disablerCol = commentContent.find(disablerString);
+  if (disablerCol != npos)
+    return true; // Disabler in comment
   return false;
-#endif // 0
 }
 
 namespace {
@@ -126,6 +142,25 @@ namespace {
     commentOss << checkerName.data();
     commentOss << "\"]";
     return commentOss.str();
+  }
+
+  bool IsDisabledByParentDecl(const Stmt * const stmt,
+                              const CheckerContext& checkerContext,
+                              const StringRef checkerName)
+  {
+    const LocationContext * const locationContext =
+      checkerContext.getLocationContext();
+    const Decl * const decl = locationContext->getDecl();
+    if (IsDisabledBySpecial(decl, checkerName))
+      return true; // Parent declaration disabled
+    return false;
+  }
+
+  bool IsDisabledBySpecial(const Decl * const decl,
+                           const StringRef checkerName) {
+    string commentString = FormDisablerString(checkerName);
+    const StringRef commentStringRef(commentString);
+    return IsCommentedWithString(decl, commentStringRef);
   }
 
   bool IsCommentedWithString(const Decl * const decl,
