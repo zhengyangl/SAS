7098e0da6274e92274a655991c37413cefc47984
Filip Bartek <fbartek@cern.ch>
2013-08-21 15:57:30 +0200

    test/const_cast.cpp: (cosmetic) Moved qualifiaction of std::string to a using declaration.

--- test/const_cast.cpp
+++ test/const_cast.cpp
@@ -6,18 +6,19 @@
 
 
 #include <string>
+std::string;
 
 
 int main()
 {
-    std::string s = "23";
-    std::string const& r_const = s;
+    string s = "23";
+    string const& r_const = s;
     
     // will produce a warning by ConstCastChecker
     // and ConstCastAwayChecker
-    std::string & r = const_cast< std::string & >( r_const );
+    string & r = const_cast< string & >( r_const );
 
-    std::string & r2 = const_cast< std::string & >( r_const ); // bug
+    string & r2 = const_cast< string & >( r_const ); // bug
 
     return 0;
 }
