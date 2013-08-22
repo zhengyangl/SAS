6104bb614bcd2f3159d0e8bb7f0fa296e548057d
Filip Bartek <fbartek@cern.ch>
2013-08-22 19:38:59 +0200

    src/StaticAccInCtorChecker.cpp: Implemented first Stmt walker. Inspiration: LLVMConventionsChecker.cpp from Clang Static Analyzer

--- src/StaticAccInCtorChecker.cpp
+++ src/StaticAccInCtorChecker.cpp
@@ -8,14 +8,53 @@ using clang::Decl;
 #include <clang/AST/DeclCXX.h>
 using clang::CXXConstructorDecl;
 
+#include <clang/AST/Stmt.h>
+using clang::Stmt;
+
+#include <clang/AST/StmtVisitor.h>
+using clang::ConstStmtVisitor;
+
 #include <clang/StaticAnalyzer/Core/PathSensitive/AnalysisManager.h>
 using clang::ento::AnalysisManager;
 
 #include <clang/StaticAnalyzer/Core/BugReporter/BugReporter.h>
 using clang::ento::BugReporter;
 
+#include <clang/AST/Expr.h>
+using clang::DeclRefExpr;
+
 #include "dbgs.h"
 using sas::dbgs;
+
+namespace {
+  class SAICVisitor : public ConstStmtVisitor<SAICVisitor> {
+  private:
+    BugReporter &BR;
+    const Decl * const DeclWithIssue;
+  public:
+    SAICVisitor(const Decl * const declWithIssue, BugReporter &br)
+      : BR(br), DeclWithIssue(declWithIssue) {}
+    void VisitChildren(const Stmt * const S) {
+      for (Stmt::const_child_iterator I = S->child_begin(), E = S->child_end();
+           I != E; ++I) {
+        if (const Stmt * const child = *I)
+          Visit(child);
+      }
+    }
+    void VisitStmt(const Stmt * const S) { VisitChildren(S); }
+    void VisitDeclRefExpr(const DeclRefExpr * const DRE);
+    //void VisitDeclStmt(DeclStmt *DS);
+  private:
+    //void VisitVarDecl(VarDecl *VD);
+  };
+} // end anonymous namespace
+
+void SAICVisitor::VisitDeclRefExpr(const DeclRefExpr * const DRE)
+{
+  // Check DRE. Does it reference a static variable?
+  //const ValueDecl * const valueDecl = DRE->getDecl();
+  return;
+}
 
 namespace sas {
   const char * const StaticAccInCtorChecker::checkerName =
@@ -26,9 +65,17 @@ namespace sas {
                                             BugReporter &BR) const
   {
     if (!D)
-      return; // invalid D
+      return; // Invalid declaration
+    if (!D->isThisDeclarationADefinition())
+      return; // No definition here
+    assert(D->hasBody());
+    const Stmt * const body = D->getBody();
+    SAICVisitor walker(D, BR);
+    walker.Visit(body);
     //D->dump();
-    dbgs() << ".";
+    //dbgs() << ".";
+    // Parse through the statements. Look for access to static data.
+    // Also parse the initializers.
   }
 
   void StaticAccInCtorChecker::checkASTCodeBody(const Decl *D,
