144c37e0c23a1e6d367de660d1100f4f64196b77
Filip Bartek <fbartek@cern.ch>
2013-08-29 19:20:48 +0200

    src/ClangSasCheckerPluginRegister.cpp: Regrouped checkers. All are now under "sas" group. Also reformatted the file to make it more readable.

--- src/ClangSasCheckerPluginRegister.cpp
+++ src/ClangSasCheckerPluginRegister.cpp
@@ -29,33 +29,60 @@
 extern "C" 
 void clang_registerCheckers ( clang::ento::CheckerRegistry &registry) 
 { 
-  registry.addChecker<sas::ConstCastAwayChecker>("threadsafety.ConstCastAway",
-						 "Check for casts which remove const qualifier");
-  registry.addChecker<sas::ConstCastChecker>("threadsafety.ConstCast",
-					     "Check for casts which remove const qualifier");
-  registry.addChecker<sas::StaticLocalChecker>("threadsafety.StaticLocal",
-					       "Check for non-const method local static variables");
-  registry.addChecker<sas::MutableMemberChecker>("threadsafety.MutableMember",
-						 "Check for members with the mutable keyword");
-  registry.addChecker<sas::GlobalStaticChecker>("threadsafety.GlobalStatic",
-						"Check for global non-const static variables");
-  registry.addChecker<sas::ClassDumperCT>("optional.ClassDumperCT",
-					  "Dump class info");
-  registry.addChecker<sas::ClassDumperFT>("optional.ClassDumperFT",
-					  "Dump class info");
-  registry.addChecker<sas::ClassDumperInherit>("optional.ClassDumperInherit",
-					       "Dump class inheritance info");
-  registry.addChecker<sas::FiniteMathChecker>("sas.NonFiniteMath",
+  registry.addChecker<sas::ConstCastAwayChecker>(
+    "sas.threadsafety.ConstCastAway",
+    "Check for casts which remove const qualifier");
+
+  registry.addChecker<sas::ConstCastChecker>(
+    "sas.threadsafety.ConstCast",
+    "Check for casts which remove const qualifier");
+
+  registry.addChecker<sas::StaticLocalChecker>(
+    "sas.threadsafety.StaticLocal",
+    "Check for non-const method local static variables");
+
+  registry.addChecker<sas::MutableMemberChecker>(
+    "sas.threadsafety.MutableMember",
+    "Check for members with the mutable keyword");
+
+  registry.addChecker<sas::GlobalStaticChecker>(
+    "sas.threadsafety.GlobalStatic",
+    "Check for global non-const static variables");
+
+  registry.addChecker<sas::ClassDumperCT>(
+    "sas.optional.ClassDumperCT",
+    "Dump class info");
+
+  registry.addChecker<sas::ClassDumperFT>(
+    "sas.optional.ClassDumperFT",
+    "Dump class info");
+
+  registry.addChecker<sas::ClassDumperInherit>(
+    "sas.optional.ClassDumperInherit",
+    "Dump class inheritance info");
+
+  registry.addChecker<sas::FiniteMathChecker>(
+    "sas.security.NonFiniteMath",
     "Check for usage of isnan and isinf");
-  registry.addChecker<sas::UsingNamespace>("sas.CodeRules.UsingNamespace",
-					   "Check for 'using namespace' or 'using std::' in header files");
-  registry.addChecker<sas::CatchAll>("sas.CodeRules.CatchAll",
-				     "Check for 'catch(...)' in source files");
-  registry.addChecker<sas::ArgSizeChecker>("performance.ArgSize",
-					   "Check for arguments passed by value with size over 4 KB");
-  registry.addChecker<sas::VarnameChecker>("sas.example.Varname",
-					   "Report variables whose names don't start with an uppercase letter");
-  registry.addChecker<sas::GlobalAccInCtorChecker>("security.GlobalAccInCtor",
+
+  registry.addChecker<sas::UsingNamespace>(
+    "sas.CodeRules.UsingNamespace",
+    "Check for 'using namespace' or 'using std::' in header files");
+
+  registry.addChecker<sas::CatchAll>(
+    "sas.CodeRules.CatchAll",
+    "Check for 'catch(...)' in source files");
+
+  registry.addChecker<sas::ArgSizeChecker>(
+    "sas.performance.ArgSize",
+    "Check for arguments passed by value with size over 4 KB");
+
+  registry.addChecker<sas::VarnameChecker>(
+    "sas.example.Varname",
+    "Report variables whose names don't start with an uppercase letter");
+
+  registry.addChecker<sas::GlobalAccInCtorChecker>(
+    "sas.security.GlobalAccInCtor",
     "Check for access to global variable in constructor");
 }
 
