5ec454c646df122d3094228d12031fc43cbf1fab
Filip Bartek <fbartek@cern.ch>
2013-08-29 14:39:26 +0200

    test.sh: Cleaned up.

--- test.sh
+++ test.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-# Filip Bartek <filip.bartek@cern.ch>
+# Author: Filip Bartek <filip.bartek@cern.ch> (2013)
 
-# Test the plugin on example files.
+# Test the SAS plugin on example files.
 
-SCANBUILD_ARG_DISABLE_CHECKERS="-disable-checker core -disable-checker cplusplus -disable-checker deadcode -disable-checker llvm -disable-checker osx -disable-checker security -disable-checker unix"
+# Requirements:
+# sas-scan-build-sas : Runs scan-build with SAS plugin loaded and only SAS
+#                      checkers enabled.
 
 cd test
 
-# All sas checkers (both threadsafe.* and sas.*):
-#sas-scan-build -o report $SCANBUILD_ARG_DISABLE_CHECKERS -enable-checker threadsafety -enable-checker sas make -B -j 32
-
-# Only threadsafe.* checkers:
-#sas-scan-build -o report $SCANBUILD_ARG_DISABLE_CHECKERS -enable-checker threadsafety make -B -j 32
-
-# threadsafe.* and sas.Varname
-#sas-scan-build -o report $SCANBUILD_ARG_DISABLE_CHECKERS -enable-checker threadsafety -enable-checker sas make -B -j 32
-
-# sas-scan-build-sas
-sas-scan-build-sas -o report make -B -j 32
+sas-scan-build-sas -o report make -B
 
 cd -
