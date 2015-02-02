80f530ccf65933a2e0d98cb5a11c1adb49daadd4
Danilo Piparo <danilo.piparo@cern.ch>
2015-02-02 02:05:34 +0100

    Adjust rule

--- src/CodingConventions/ROOT/RN13Checker.cpp
+++ src/CodingConventions/ROOT/RN13Checker.cpp
@@ -17,8 +17,7 @@ namespace sas
          void RN13Checker::checkASTDecl(const clang::EnumConstantDecl* D, clang::ento::AnalysisManager& Mgr, clang::ento::BugReporter& BR) const
          {
             auto declName=D->getName();
-            if (declName.size()<2 ||
-                (declName.startswith("k") && !std::isupper(declName[1]))) return;
+            if (declName.size()>2 && declName[0]=='k' && std::isupper(declName[1])) return;
             Report(D, "RN6: Enumeration constants names must start with \"k\" and the second letter must be uppercase.", BR);
          }
       }
