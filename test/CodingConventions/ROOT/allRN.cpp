3374d9fa84c6819c2fb8b1b7bdf9499106b5024a
Danilo Piparo <danilo.piparo@cern.ch>
2015-02-09 23:46:21 +0100

    Test all ROOT coding conventions checkers

--- test/CodingConventions/ROOT/allRN.cpp
+++ test/CodingConventions/ROOT/allRN.cpp
@@ -26,4 +26,19 @@ auto ul = (unsigned long) &myDouble;
 auto l = (long) &myDouble;
 auto ull = (unsigned long long) &myDouble;
 auto ll = (long long) &myDouble;
+int* a=nullptr;
+if (!a) return 1;
+
 }
+
+int wrongName=3;
+float gCorrectName=2.f;
+
+class A{
+int fCorrectName; // member
+double fwrongName; // member
+void* WrongName; // member
+static int fgCorrectName; // static member
+static int fWrongName; // static member
+};
+
