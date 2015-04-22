5e14a107b5ddc2b30c520341d3f87973f87e1733
Danilo Piparo <danilo.piparo@cern.ch>
2015-04-22 10:02:23 +0200

    Refactor a bit the testing

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,29 +1,42 @@
 set (CTEST_PROJECT_NAME "SAS")
 
-macro(add_sas_test TEST_NAME TEST_SOURCE TEST_ENABLED_CHECKERS TEST_EXPECT_OUTPUT)
+macro(add_sas_test TEST_NAME TEST_SOURCE)
    set(SAS_TEST_SCRIPT "${CMAKE_SOURCE_DIR}/scripts/test.py")
    set(SAS_CLANG "${CMAKE_BINARY_DIR}/scripts/clang")
    set(SAS_CLANGXX "${CMAKE_BINARY_DIR}/scripts/clang++")
    set(SAS_BINARY "${CMAKE_BINARY_DIR}/lib/libSas.so")
 
-   add_test(NAME "${TEST_NAME}" COMMAND "python" "${SAS_TEST_SCRIPT}" "${SAS_CLANGXX}" "${CMAKE_SOURCE_DIR}/test/${TEST_SOURCE}" "${SAS_BINARY}" "${TEST_ENABLED_CHECKERS}" "${TEST_EXPECT_OUTPUT}")
+   # By default use the name of the test
+   set(TEST_ENABLED_CHECKERS ${TEST_NAME})
+
+   set(FULL_TEST_SOURCE "${CMAKE_SOURCE_DIR}/test/${TEST_SOURCE}")
+   set(FULL_TEST_REF "${CMAKE_SOURCE_DIR}/test/${TEST_SOURCE}")
+   string(REPLACE ".cpp" ".ref" FULL_TEST_REF ${FULL_TEST_SOURCE})
+
+   add_test(NAME "${TEST_NAME}"
+            COMMAND "python" "${SAS_TEST_SCRIPT}" "${SAS_CLANGXX}" "${FULL_TEST_SOURCE}" "${SAS_BINARY}" "${TEST_ENABLED_CHECKERS}" "${FULL_TEST_REF}"
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/Testing")
 endmacro(add_sas_test)
 
 enable_testing()
-add_sas_test("sas.CodingConventions.ROOT.RN3"  "CodingConventions/ROOT/RN3.cpp"  "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN3]")
-add_sas_test("sas.CodingConventions.ROOT.RN4"  "CodingConventions/ROOT/RN4.cpp"  "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN4]")
-add_sas_test("sas.CodingConventions.ROOT.RN6"  "CodingConventions/ROOT/RN6.cpp"  "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN6]")
-add_sas_test("sas.CodingConventions.ROOT.RN9"  "CodingConventions/ROOT/RN9.cpp"  "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN9]")
-add_sas_test("sas.CodingConventions.ROOT.RN10" "CodingConventions/ROOT/RN10.cpp" "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN10]")
-add_sas_test("sas.CodingConventions.ROOT.RN11" "CodingConventions/ROOT/RN11.cpp" "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN11]")
-add_sas_test("sas.CodingConventions.ROOT.RN12" "CodingConventions/ROOT/RN12.cpp" "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.RN12]")
-add_sas_test("sas.CodingConventions.ROOT.PtrCastWinChecker"  "CodingConventions/ROOT/ptr_cast_win.cpp"  "sas.CodingConventions.ROOT" "[sas.CodingConventions.ROOT.PtrCastWinChecker]")
-add_sas_test("sas.CodingConventions.General.NoUsingNamespaceInHeaders" "CodingConventions/General/using_namespace.cpp"  "sas.CodingConventions.General" "[sas.CodingConventions.General.NoUsingNamespaceInHeaders]")
-add_sas_test("sas.ThreadSafety.ConstCastAwayChecker" "ThreadSafety/const_cast_away.cpp" "sas.ThreadSafety" "[sas.ThreadSafety.ConstCastAwayChecker]")
-add_sas_test("sas.ThreadSafety.ConstCastChecker"  "ThreadSafety/const_cast.cpp"  "sas.ThreadSafety" "[sas.ThreadSafety.ConstCastChecker]")
-add_sas_test("sas.ThreadSafety.GlobalStaticChecker" "ThreadSafety/global_static.cpp" "sas.ThreadSafety" "[sas.ThreadSafety.GlobalStaticChecker]")
-add_sas_test("sas.ThreadSafety.StaticLocalChecker" "ThreadSafety/static_local.cpp" "sas.ThreadSafety" "[sas.ThreadSafety.StaticLocalChecker]")
-add_sas_test("sas.ThreadSafety.MutableMemberChecker" "ThreadSafety/mutable_member.cpp" "sas.ThreadSafety" "[sas.ThreadSafety.MutableMemberChecker]")
-add_sas_test("sas.Performance.ArgSizeChecker"  "Performance/arg_size.cpp" "sas.Performance" "[sas.Performance.ArgSizeChecker]")
-add_sas_test("sas.Performance.FiniteMathChecker" "Performance/finite_math.cpp" "sas.Performance" "[sas.Performance.FiniteMathChecker]")
-add_sas_test("sas.Example.Varname"  "Example/varname.cpp" "sas.Example" "[sas.Example.Varname]")
+
+add_sas_test("sas.CodingConventions.ROOT.RN3"                          "CodingConventions/ROOT/RN3.cpp")
+add_sas_test("sas.CodingConventions.ROOT.RN4"                          "CodingConventions/ROOT/RN4.cpp")
+add_sas_test("sas.CodingConventions.ROOT.RN6"                          "CodingConventions/ROOT/RN6.cpp")
+add_sas_test("sas.CodingConventions.ROOT.RN9"                          "CodingConventions/ROOT/RN9.cpp")
+add_sas_test("sas.CodingConventions.ROOT.RN10"                         "CodingConventions/ROOT/RN10.cpp")
+add_sas_test("sas.CodingConventions.ROOT.RN11"                         "CodingConventions/ROOT/RN11.cpp")
+add_sas_test("sas.CodingConventions.ROOT.RN12"                         "CodingConventions/ROOT/RN12.cpp")
+add_sas_test("sas.CodingConventions.ROOT.PtrCastWin"                   "CodingConventions/ROOT/ptr_cast_win.cpp")
+add_sas_test("sas.CodingConventions.General.NoUsingNamespaceInHeaders" "CodingConventions/General/using_namespace.cpp")
+
+add_sas_test("sas.ThreadSafety.ConstCastAway" "ThreadSafety/const_cast_away.cpp")
+add_sas_test("sas.ThreadSafety.ConstCast"     "ThreadSafety/const_cast.cpp")
+add_sas_test("sas.ThreadSafety.GlobalStatic"  "ThreadSafety/global_static.cpp")
+add_sas_test("sas.ThreadSafety.StaticLocal"   "ThreadSafety/static_local.cpp")
+add_sas_test("sas.ThreadSafety.MutableMember" "ThreadSafety/mutable_member.cpp")
+
+add_sas_test("sas.Performance.ArgSizeChecker"    "Performance/arg_size.cpp")
+add_sas_test("sas.Performance.FiniteMathChecker" "Performance/finite_math.cpp")
+
+add_sas_test("sas.Example.Varname"  "Example/varname.cpp")
--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,42 @@
 #! /usr/bin/env python
 
 import sys
+import os
 import subprocess
+import difflib
+
+def getBasePath(sas_plugin):
+   """
+   Get the path specific to the SAS installation
+   """
+   scriptPath = os.path.realpath(__file__)
+   return scriptPath.replace("/scripts/test.py","")
+
+def cleanOutput(specificPath,analyze_output):
+   return analyze_output.replace(specificPath,"")
+
+def printDiff(str1,str2):
+   diff = difflib.ndiff(str1.splitlines(1), str2.splitlines(1))
+   print ''.join(diff),
+
+def runTest():
+   cc, src, sas_plugin, checkers, refOutputName = sys.argv[1:]
+   basePath = getBasePath(sas_plugin)
+
+   compile_env = {"SA_PLUGIN": sas_plugin, "SA_CHECKERS": checkers}
+   process = subprocess.Popen([cc, "-c", "-std=c++11", src], stderr=subprocess.PIPE, env=compile_env)
+
+   analyze_output = process.communicate()[1]
+   clean_analyze_output = cleanOutput(basePath, analyze_output)
+
+   refOutput = open(refOutputName).read()
+
+   if refOutput == clean_analyze_output:
+      return 0
+   else:
+      printDiff(refOutput,clean_analyze_output)
+      return 1
 
 if __name__ == "__main__":
-   cc, src, sas_binary, checkers, sample_output = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]
-   compile_env = {"SA_PLUGIN": sas_binary, "SA_CHECKERS": checkers}
-   process = subprocess.Popen([cc, "-c", "-std=c++11", src], stderr=subprocess.PIPE, env=compile_env)
-   analyze_output = process.communicate()[1]
-   print analyze_output
-   if sample_output in analyze_output:
-      sys.exit(0)
-   else:
-      sys.exit(1)
+   exitCode = runTest()
+   sys.exit(exitCode)
