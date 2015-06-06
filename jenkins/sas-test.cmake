6ba77e41dc26ea28ebb50a9fd11214f0e5d07d98
Danilo Piparo <danilo.piparo@cern.ch>
2015-06-06 15:47:56 +0200

    Add ctest setup for cdash

---
+++ jenkins/sas-test.cmake
@@ -0,0 +1,9 @@
+
+set(CTEST_PROJECT_NAME "VecGeom")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "cdash.cern.ch")
+set(CTEST_DROP_LOCATION "/submit.php?project=VecGeom")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
